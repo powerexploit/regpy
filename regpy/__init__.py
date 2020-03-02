ddc0efc63207f62a813372bb3901e2e95ec7d2f6
ankitdobhal <dobhal.ankit@protonmail.com>
2020-03-02 18:18:40 +0530

    Added regpy file

---
+++ regpy/__init__.py
@@ -0,0 +1,56 @@
+from re import findall
+
+# text function to grab all alphabetic data.
+def text(data):
+	pattern = r'[a-zA-Z]'
+	expression = findall(pattern,data)
+	if expression:
+		print(','.join(expression))
+	else:
+		return False
+
+# emails function to grab all valid mails id's & join them. 
+def emails(data):
+	pattern = r'[a-zA-Z0-9_]+@[a-zA-Z0-9]+\.[a-z]{1,3}'
+	expression = findall(pattern,data)
+	if expression:
+		print('-'.join(expression))
+	else:
+		return False
+		
+# everything function will match everything except alphanumeric data.
+def everything(data):
+	pattern = r'[^a-zA-Z0-9]'
+	expression = findall(pattern,data)
+	if expression:
+		return expression
+	else:
+		return False
+
+# postal_code function to grab all valid postal code in list.
+def postal_code(data):
+	pattern = r'^[1-9]\d{5}$'
+	expression = findall(pattern,data)
+	if expression:
+		return expression
+	else:
+		return False
+
+
+# url function to grab all urls in list.
+def url(data):
+	pattern = r'https://[a-zA-Z0-9]+\.[a-z]'
+	expression = findall(pattern,data)
+	if expression:
+		return expression
+	else:
+		return False
+
+# ip function to grab all ip's in list.
+def ip(data):
+	pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+	expression = findall(pattern,data)
+	if expression:
+		return expression
+	else:
+		return False
